7142e90be6d363a9ac979950b28ec09fbe55767c
codefortechnology <yogeshsainani.it21@gmail.com>
2022-09-03 18:31:18 +0530

    add logger

--- src/main/java/com/project/project/controller/Student.java
+++ src/main/java/com/project/project/controller/Student.java
@@ -1,6 +1,8 @@
 package com.project.project.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class Student {
+
+    Logger logger = LoggerFactory.getLogger(Student.class);
     @GetMapping("student")
     public String welcome(){
+        logger.info("controller logger");
         return "hello student";
     }
 }
--- src/test/java/com/project/project/ProjectApplicationTests.java
+++ src/test/java/com/project/project/ProjectApplicationTests.java
@@ -1,13 +1,20 @@
 package com.project.project;
 
+import com.project.project.controller.Student;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class ProjectApplicationTests {
-
+	Logger logger = LoggerFactory.getLogger(Student.class);
 	@Test
 	void contextLoads() {
+		logger.info("test logger");
 	}
 
 }
