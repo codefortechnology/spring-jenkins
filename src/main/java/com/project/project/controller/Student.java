99e261867044b74a658a57f981e0da0b54c71474
codefortechnology <yogeshsainani.it21@gmail.com>
2022-09-03 14:40:58 +0530

    add rest

---
+++ src/main/java/com/project/project/controller/Student.java
@@ -0,0 +1,15 @@
+package com.project.project.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Student {
+    @GetMapping("student")
+    public String welcome(){
+        return "hello student";
+    }
+}
